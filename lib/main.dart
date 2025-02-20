0f546c2933cc32dea43ad34db6ec5728220bad8a
GAEL_ROGELIO_BEJARANO_DOMINGUEZ <a.22308051281154@cbtis128.edu.mx>
2025-02-20 19:35:49 +0000

    Enviando Widgets con columnas

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyWidgets());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyWidgets extends StatelessWidget {
+  const MyWidgets({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text(
+              "Bejarano",
+              style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 32,
+                  color: Color(0xffffffff)),
+            ),
+            backgroundColor: Color(0xffff96cb),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.search),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              )
+            ],
+            leading: const Icon(
+              Icons.catching_pokemon_outlined,
+              color: Colors.white,
+            )),
+        body: const Column(
+          children: <Widget>[
+            Text(
+              'Rogelio Bejarano',
+              style: TextStyle(fontSize: 24, color: Color(0xff00afff)),
+            ),
+            Text(
+              'Mat: 22308051281154 Gpo: 6J',
+              style: TextStyle(fontSize: 24, color: Color(0xffc632ff)),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
         ),
       ),
     );
